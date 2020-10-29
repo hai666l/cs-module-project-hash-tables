a3be67f978f6a3b5379f09a6e0eb5f4e483e9514
hai666l <0xconr@protonmail.com>
2020-10-28 21:52:36 -0500

    Passing Day 1 Tests

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -7,43 +7,21 @@ class HashTableEntry:
         self.value = value
         self.next = None
 
-
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
 
-
 class HashTable:
-    """
-    A hash table that with `capacity` buckets
-    that accepts string keys
-
-    Implement this.
-    """
-
     def __init__(self, capacity):
-        # Your code here
-
+        self.storage = [None] * capacity
 
     def get_num_slots(self):
-        """
-        Return the length of the list you're using to hold the hash
-        table data. (Not the number of items stored in the hash table,
-        but the number of slots in the main list.)
-
-        One of the tests relies on this.
-
-        Implement this.
-        """
-        # Your code here
-
+        return len(self.storage)
 
     def get_load_factor(self):
-        """
-        Return the load factor for this hash table.
-
-        Implement this.
-        """
-        # Your code here
+        # load factor: a = n/m
+        m = len(self.storage)
+        n = m - self.storage.count(None)
+        return n/m
 
 
     def fnv1(self, key):
@@ -52,58 +30,35 @@ class HashTable:
 
         Implement this, and/or DJB2.
         """
+        FNV_offset_basis = 14695981039346656037
+        FNV_prime = 1099511628211
+		
+        fnv1_hash = FNV_offset_basis
 
-        # Your code here
-
+        for byte in key.encode():
+            fnv1_hash *= FNV_prime
+            fnv1_hash ^= byte
+		
+        return fnv1_hash
 
     def djb2(self, key):
-        """
-        DJB2 hash, 32-bit
-
-        Implement this, and/or FNV-1.
-        """
-        # Your code here
+        pass
 
 
     def hash_index(self, key):
-        """
-        Take an arbitrary key and return a valid integer index
-        between within the storage capacity of the hash table.
-        """
-        #return self.fnv1(key) % self.capacity
-        return self.djb2(key) % self.capacity
+        return self.fnv1(key) % len(self.storage)
+        #return self.djb2(key) % self.capacity
 
     def put(self, key, value):
-        """
-        Store the value with the given key.
-
-        Hash collisions should be handled with Linked List Chaining.
-
-        Implement this.
-        """
-        # Your code here
+        self.storage[self.hash_index(key)] = value
 
 
     def delete(self, key):
-        """
-        Remove the value stored with the given key.
-
-        Print a warning if the key is not found.
-
-        Implement this.
-        """
-        # Your code here
+        self.storage[self.hash_index(key)] = None
 
 
     def get(self, key):
-        """
-        Retrieve the value stored with the given key.
-
-        Returns None if the key is not found.
-
-        Implement this.
-        """
-        # Your code here
+        return self.storage[self.hash_index(key)]
 
 
     def resize(self, new_capacity):
@@ -134,7 +89,10 @@ if __name__ == "__main__":
     ht.put("line_12", "And stood awhile in thought.")
 
     print("")
+    print("self.storage:")
+    print(ht.storage)
 
+    """
     # Test storing beyond capacity
     for i in range(1, 13):
         print(ht.get(f"line_{i}"))
@@ -151,3 +109,4 @@ if __name__ == "__main__":
         print(ht.get(f"line_{i}"))
 
     print("")
+    """
