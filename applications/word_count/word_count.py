a959d4c6456267a73f7e7499e856b93b9e097edb
hai666l <0xconr@protonmail.com>
2020-11-05 12:41:08 -0600

    word_count/

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,7 +1,20 @@
 def word_count(s):
-    # Your code here
+    s = s.lower()
+    ignore = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&' ]
 
+    # Split string by spaces and NULL terminator into a list, remove ignore chars from each word in that list
+    words = s.split()
+    for i in range(0, len(words)):
+        for c in ignore:
+            words[i] = words[i].replace(c, '')
 
+    # Prepare a dict count of each word in the list 
+    out = {}
+    for word in words:
+        if word != "":
+            out[word] = words.count(word)
+
+    return out
 
 if __name__ == "__main__":
     print(word_count(""))
