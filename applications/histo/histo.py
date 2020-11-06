fe79f908fc069fe4f166c20cb731d5341149e8bd
hai666l <0xconr@protonmail.com>
2020-11-05 20:04:53 -0600

    histo/, not sorted alphabetically

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,21 @@
-# Your code here
+from word_count import word_count
 
+def histogram(filename):
+    with open(filename) as f:
+        contents = f.read().lower()
+        d = word_count(contents)
+        # Sort dict by values, highest to lowest
+        d = { k: v for k, v in sorted(d.items(), key=lambda ele: ele[1], reverse=True) }
+
+        # Find longest word and use its length+2 as padding
+        padding = 1
+        for w in d.keys():
+            if len(w) > padding:
+                padding = len(w) + 2
+
+        for word in d.keys():
+            print(word , end="")
+            print(' ' * (padding-len(word)), end="")
+            print('#' * d[word])
+
+histogram("robin.txt")
