355191a7c55452c676f0f2e8923e3f987bb58f8d
hai666l <0xconr@protonmail.com>
2020-11-05 19:08:51 -0600

    Refactor markov/

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,25 +1,55 @@
 import random
 
+# Helpers
+def isStartWord(w):
+    if w != "":
+        if w[0] == '"': return True
+        if w[0].isupper(): return True
+    return False
+
+def isStopChar(char):
+    for c in ".?!":
+        if char == c:
+            return True
+    return False
+
+def isStopWord(w):
+    if len(w) >= 2:
+        if isStopChar(w[-1]) or (w[-1] == '"' and isStopChar(w[-2])):
+            return True
+    return False
+
+# Open and read input text
 d = dict()
 with open("input.txt") as f:
     words = f.read().split()
     for i in range(0, len(words)):
         w = words[i]
+        # Add word to dictionary
         if w not in d.keys():
+            # If end of text not reached
             if i+1 != len(words):
                 d[w] = [words[i+1]]
+            # End of text reached, add a random word as follower
             else:
                 d[w] = [random.choice(words)]
+        # Word already in dictionary
         else:
+            # Add the word following to array
             if i+1 != len(words):
                 d[w].append(words[i+1])
             
 # construct random sentence
-start = random.choice(list(d.keys()))
-stop = random.choice(list(d.keys()))
-
+start = ""
+while isStartWord(start) != True:
+    start = random.choice(list(d.keys()))
 randomWord = start
-while randomWord != stop:
+i = 0
+while i != 5:
     print(randomWord, end=' ')
     randomWord = random.choice(d[randomWord])
-print(stop)
+    if isStopWord(randomWord):
+        i += 1
+        print(f'{randomWord}\n')
+        while isStartWord(randomWord) != True:
+            randomWord = random.choice(list(d.keys()))
